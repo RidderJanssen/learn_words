34a7faccd43475494e0187fa8adedf3dbef4c869
Ridder Janssen <ridderjanssen@gmail.com>
2017-07-28 22:15:46 +0200

    Possible to add words to the list

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,12 @@ class WordsController < ApplicationController
   def show
     @words = Word.all
   end
+  def new
+  end
+  def create
+    @word = Word.new(params.require(:word).permit(:meaninga, :meaningb))
+    
+    @word.save
+    redirect_to @word
+  end
 end
